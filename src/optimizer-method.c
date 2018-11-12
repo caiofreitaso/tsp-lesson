89dfc2652c9e72e247e28f0f1a01f82e0b86c32b
Caio Freitas de Oliveira <caio@tie-bomber>
2018-11-12 15:41:18 -0200

    use OptimizerOutput

--- src/optimizer-method.c
+++ src/optimizer-method.c
@@ -4,24 +4,24 @@ int is_valid(Solution this, int size) {
   return is_cycle(this,size) && only_valid_elements(this,size);
 }
 
-void branch(Solution* best, int* best_value, Solution* current, Matrix distance, int size, int i, int value) {
-  (*current)[i] = value;
-  recursive(best, best_value, current, distance, size, i+1);
-}
-
-void leaf(Solution* best, int* best_value, Solution* current, Matrix distance, int size) {
-  if (is_valid(*current, size)) {
-    update_best(best, best_value, *current, distance, size);
+void branch(OptimizerOutput* best, Solution* current, Matrix distance, int size, int i) {
+  int j;
+  for (j = 0; j < size; j++) {
+    (*current)[i] = j;
+    recursive(best, current, distance, size, i+1);
   }
 }
 
-void recursive(Solution* best, int* best_value, Solution* current, Matrix distance, int size, int i) {
+void leaf(OptimizerOutput* best, Solution* current, Matrix distance, int size) {
+  if (is_valid(*current, size)) {
+    update_best(best, *current, distance, size);
+  }
+}
+
+void recursive(OptimizerOutput* best, Solution* current, Matrix distance, int size, int i) {
   if (i < size - 1) {
-    int j;
-    for (j = 0; j < size; j++) {
-      branch(best, best_value, current, distance, size, i, j);
-    }
+    branch(best, current, distance, size, i);
   } else {
-    leaf(best, best_value, current, distance, size);
+    leaf(best, current, distance, size);
   }
 }
